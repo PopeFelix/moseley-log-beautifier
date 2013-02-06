31160456252d6edb3b908e4c0915f987ef3e5cf0
cpeters <cpeters@5edcdadc-f7b2-6344-b779-4e7e72d6ce73>
2013-02-06 22:12:01 +0000

    Ran code through perlcritic and perltidy.  Corrected or explicitly ignored all violations found by perlcritic.

--- moseley-log-beautifier.pl
+++ moseley-log-beautifier.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# A script to beautify automatic log output from Moseley CommServer by 
+# A script to beautify automatic log output from Moseley CommServer by
 # presenting the supplied data in a more human readable form.
 #
 # Original author: Kit Peters <cpeters@ucmo.edu>
@@ -14,7 +14,9 @@
 use strict;
 use warnings;
 
+use feature qw/switch/;
 use autodie;
+use charnames q/:full/;
 use English qw/-no_match_vars/;
 use Text::CSV_XS;
 use Time::Piece;
@@ -27,102 +29,127 @@ use File::Spec;
 use Clone qw/clone/;
 use Cwd;
 
+our $VERSION = 1.0;
+
+Readonly my $FUNCTION_NAME_POSITION = 3;
 Readonly my %DEFAULTS => (
     '_' => {
-        'channels_file' => q/channels.ini/,
+        'channels_file'       => q/channels.ini/,
         'transmitter_log_dir' => q|Z:|,
-        'printer_path' => q|//153.91.87.132/HP DeskJet 712C|,
-        'field_order'=> [qw/T33 T34 T41 T48 T32 S1/],
-        'print_with_word' => 1,
-        'header_file' => 'header.txt',
-        'footer_file' => 'footer.txt',
+        'printer_path'        => q|//153.91.87.132/HP DeskJet 712C|,
+        'field_order'         => [qw/T33 T34 T41 T48 T32 S1/],
+        'print_with_word'     => 1,
+        'header_file'         => 'header.txt',
+        'footer_file'         => 'footer.txt',
     },
 );
-Readonly my $CONFIG_FILE          => q/moseley-log-beautifier.ini/;
-Readonly my $CONFIG               => get_configuration($CONFIG_FILE);
-if (!$CONFIG) {
-    die qq/Failed to load config file "$CONFIG_FILE": / . Config::Tiny->errstr;
+Readonly my $CONFIG_FILE => q/moseley-log-beautifier.ini/;
+Readonly my $CONFIG      => get_configuration($CONFIG_FILE);
+
+if ( !$CONFIG ) {
+    croak( qq/Failed to load config file "$CONFIG_FILE": /
+          . Config::Tiny->errstr );
 }
-Readonly my $CHANNELS             => get_channels($CONFIG->{'_'}{'channels_file'});
+Readonly my $CHANNELS => get_channels( $CONFIG->{'_'}{'channels_file'} );
 
 main();
 1;
 
 sub main {
-    # FIXME: figure out if I'm opening the file dated the night before or what.
-    my $logfile = $ARGV[0] || File::Spec->catfile($CONFIG->{'_'}{'transmitter_log_dir'}, q/Log.txt/);
+
+    my $logfile = $ARGV[0]
+      || File::Spec->catfile( $CONFIG->{'_'}{'transmitter_log_dir'},
+        q/Log.txt/ );
 
     open my $fh, '<', $logfile;
     my $processed_records = _process_transmitter_log($fh);
     close $fh;
-   
-    my $log_date = [sort keys %{$processed_records}]->[0];
-    print_processed_logs({ 'log_date' => $log_date, 'log_data' => $processed_records});
+
+    my $log_date = [ sort keys %{$processed_records} ]->[0];
+    print_processed_logs(
+        { 'log_date' => $log_date, 'log_data' => $processed_records } );
     return 1;
 }
 
 sub print_processed_logs {
     my $args = shift;
-    
-    if (ref $args ne q/HASH/) {
-        croak(sprintf(q/Usage: %s <hashref>/, (caller(0))[3]));
+
+    if ( ref $args ne q/HASH/ ) {
+        croak(
+            sprintf q/Usage: %s <hashref>/,
+            ( caller 0 )[$FUNCTION_NAME_POSITION]
+        );
     }
     foreach my $required_key (qw/log_date log_data/) {
-        if (!$args->{$required_key}) {
+        if ( !$args->{$required_key} ) {
             croak(qq/Missing required key '$required_key' in args/);
         }
     }
 
-    my $tabular_data = _format_tabular($args->{'log_data'});
+    my $tabular_data = _format_tabular( $args->{'log_data'} );
 
-    if ($CONFIG->{'_'}{'print_with_word'}) {
-        _print_with_word({'log_date' => $args->{'log_date'}, 'data' => $tabular_data});
-    } 
+    if ( $CONFIG->{'_'}{'print_with_word'} ) {
+        _print_with_word(
+            { 'log_date' => $args->{'log_date'}, 'data' => $tabular_data } );
+    }
     else {
-        _print_as_text({'log_date' => $args->{'log_date'}, 'data' => $tabular_data});
+        _print_as_text(
+            { 'log_date' => $args->{'log_date'}, 'data' => $tabular_data } );
     }
     return 1;
 }
 
 sub _format_tabular {
     my $horizontal_records = shift;
-    
-    if (ref $horizontal_records ne q/HASH/) {
-        croak(sprintf(q/Usage: %s <hashref>/, (caller(0))[3]));
+
+    if ( ref $horizontal_records ne q/HASH/ ) {
+        croak(
+            sprintf q/Usage: %s <hashref>/,
+            ( caller 0 )[$FUNCTION_NAME_POSITION]
+        );
     }
-    
-    my @output_fields = map { $_->{'Description'} } @{$CHANNELS}{ @{ $CONFIG->{'_'}{'field_order'} } };
+
+    my @output_fields = map { $_->{'Description'} }
+      @{$CHANNELS}{ @{ $CONFIG->{'_'}{'field_order'} } };
     my $output_formats = {};
-    foreach my $key (keys %{$CHANNELS}) {
+    foreach my $key ( keys %{$CHANNELS} ) {
         my $field_name = $CHANNELS->{$key}{'Description'};
-        my $units = $CHANNELS->{$key}{'Units'};
+        my $units      = $CHANNELS->{$key}{'Units'};
         $output_formats->{$field_name} = $units;
     }
-    my @tabular = ([q|Time|, @output_fields]); # initialize w/ column headings
-    foreach my $timestamp (sort keys %{$horizontal_records}) {
-        my $record = $horizontal_records->{$timestamp};
-      
-        (undef, my $time) = split /\s/, $timestamp, 2; # throw away the date portion of the timestamp
+    my @tabular =
+      ( [ q|Time|, @output_fields ] );    # initialize w/ column headings
+    foreach my $timestamp ( sort keys %{$horizontal_records} ) {
+        my $horizontal_record = $horizontal_records->{$timestamp};
+
+        # Extract the time portion of the timestamp
+        my ($time) = $timestamp =~ m/(\d{2}:\d{2}:\d{2}/xsm;
+
         # Add units to the tabular data
         foreach my $field_name (@output_fields) {
             my $unit = $output_formats->{$field_name};
-            if ($unit =~ /none/ixsm) {
-                next;
-            }
-            elsif ($unit =~ /bool/ixsm) {
-                $record->{$field_name} = ($record->{$field_name}) ? 'YES' : 'NO';
-            } 
-            elsif ($unit =~ /percent/ixsm)  {
-                $record->{$field_name} .= '%';
-            }
-            elsif ($unit =~ /deg/ixsm) {
-                $record->{$field_name} .= qq/\xB0/; # degree symbol - NOTE: UTF-8
-            }
-            else {
-                $record->{$field_name} .= uc(substr($unit, 0, 1));
+
+            given ($unit) {
+                when (/^none/ixsm) {
+                    next;
+                }
+                when (/^bool/ixsm) {
+                    $horizontal_record->{$field_name} =
+                      ( $horizontal_record->{$field_name} ) ? 'YES' : 'NO';
+                }
+                when (/^percent/ixsm) {
+                    $horizontal_record->{$field_name} .= q{%};
+                }
+                when (/^deg/ixsm) {
+                    $horizontal_record->{$field_name} .=
+                      qq/\N{DEGREE SIGN}/;    # NOTE: UTF-8
+                }
+                default {
+                    $horizontal_record->{$field_name} .= uc substr $unit, 0, 1;
+                }
             }
         }
-        push @tabular, [$time, @{$record}{@output_fields}];
+        push @tabular, [ $time, @{$horizontal_record}{@output_fields} ];
     }
 
     return \@tabular;
@@ -130,59 +157,77 @@ sub _format_tabular {
 
 # Expects arguments as a hashref with the keys:
 # # log_date: Date of the log
-# # data: an arrayref of arrayrefs.  First line is treated as column headings, following lines are treated as data.  
+# # data: an arrayref of arrayrefs.  First line is treated as column headings, following lines are treated as data.
 #
 # A double horizontal rule will be added between the column headings and the data.
 sub _print_with_word {
     my $args = shift;
-    
-    if (ref $args ne q/HASH/) {
-        croak(sprintf(q/Usage: %s <hashref>/, (caller(0))[3]));
+
+    if ( ref $args ne q/HASH/ ) {
+        croak(
+            sprintf q/Usage: %s <hashref>/,
+            ( caller 0 )[$FUNCTION_NAME_POSITION]
+        );
     }
     foreach my $required_key (qw/log_date data/) {
-        if (!$args->{$required_key}) {
+        if ( !$args->{$required_key} ) {
             croak(qq/Missing required key '$required_key' in args/);
         }
     }
 
-    my $header = _slurp_file($CONFIG->{'_'}{'header_file'});
-    my $footer = _slurp_file($CONFIG->{'_'}{'footer_file'});
-    my $csv = Text::CSV_XS->new({ 'sep_char' => ',', 'binary' => 1, 'quote_char' => undef });
+    my $header = _slurp_file( $CONFIG->{'_'}{'header_file'} );
+    my $footer = _slurp_file( $CONFIG->{'_'}{'footer_file'} );
+    my $csv    = Text::CSV_XS->new(
+        { 'sep_char' => q/,/, 'binary' => 1, 'quote_char' => undef } );
 
-    my @column_headings = @{shift $args->{'data'}};
-    my @rows = @{$args->{'data'}};
+    my @column_headings = @{ shift $args->{'data'} };
+    my @rows            = @{ $args->{'data'} };
 
-    my $word = Win32::OLE->new('Word.Application', 'Quit');
-    my $doc = $word->Documents->Add();
+    my $word   = Win32::OLE->new( 'Word.Application', 'Quit' );
+    my $doc    = $word->Documents->Add();
     my $select = $word->Selection;
 
+    ## no critic (ProhibitLongChainsOfMethodCalls)
+# Explanation: I'm working with the MS Word OLE API.  The way that this API is set up requires long chains of method calls
+# to drill down to the particular object I need to work with.
     $select->ParagraphFormat->{'SpaceAfter'} = 0;
-    $select->TypeText({'Text' => qq/$header\n\n/,});
+    $select->TypeText( { 'Text' => qq/$header\n\n/, } );
     $select->BoldRun();
     $select->ParagraphFormat->{'Alignment'} = wdAlignParagraphRight;
-    $select->TypeText({'Text' => Time::Piece->strptime($args->{'log_date'}, q|%m/%d/%Y %H:%M:%S|)->strftime(qq/%A %B %d %Y\n\n/)});
+    $select->TypeText(
+        {
+            'Text' => Time::Piece->strptime(
+                $args->{'log_date'}, q|%m/%d/%Y %H:%M:%S|
+            )->strftime(qq/%A %B %d %Y\n\n/)
+        }
+    );
     $select->BoldRun();
 
     $csv->combine(@column_headings);
-    $select->InsertAfter($csv->string);
+    $select->InsertAfter( $csv->string );
     $select->InsertParagraphAfter;
     for my $row (@rows) {
-        $csv->combine(@{$row});
-        $select->InsertAfter($csv->string);
+        $csv->combine( @{$row} );
+        $select->InsertAfter( $csv->string );
         $select->InsertParagraphAfter;
     }
 
-    my $table = $select->ConvertToTable({'Separator' => wdSeparateByCommas});
+    my $table =
+      $select->ConvertToTable( { 'Separator' => wdSeparateByCommas } );
     $table->Rows->First->Range->Font->{'Bold'} = 1;
-    $table->Rows->First->Range->ParagraphFormat->{'Alignment'} = wdAlignParagraphCenter;
-    @{$table->Rows->First->Borders(wdBorderBottom)}{qw/LineStyle LineWidth/} = (wdLineStyleDouble, wdLineWidth100pt);
-    $doc->Paragraphs->Last->Format->{'Alignment'} = wdAlignParagraphLeft;
+    $table->Rows->First->Range->ParagraphFormat->{'Alignment'} =
+      wdAlignParagraphCenter;
+    @{ $table->Rows->First->Borders(wdBorderBottom) }{qw/LineStyle LineWidth/}
+      = ( wdLineStyleDouble, wdLineWidth100pt );
+    $doc->Paragraphs->Last->Format->{'Alignment'}  = wdAlignParagraphLeft;
     $doc->Paragraphs->Last->Format->{'SpaceAfter'} = 0;
-    $doc->Paragraphs->Last->Range->InsertAfter({'Text' => qq/\n$footer/}); 
-    $doc->SaveAs({ 'Filename' => Cwd::getcwd . '/test.doc' });
-#    $doc->PrintOut();
-    $doc->Close({ 'SaveChanges' => wdDoNotSaveChanges });
+    $doc->Paragraphs->Last->Range->InsertAfter( { 'Text' => qq/\n$footer/ } );
+    $doc->SaveAs( { 'Filename' => Cwd::getcwd . '/test.doc' } );
+
+    #    $doc->PrintOut();
+    $doc->Close( { 'SaveChanges' => wdDoNotSaveChanges } );
     $word->Quit();
+    ## use critic
     return 1;
 }
 
@@ -190,27 +235,30 @@ sub _slurp_file {
     my $file = shift;
 
     open my $fh, '<', $file;
-    my $text = do { local ($/); <$fh> };
+    my $text = do { local $INPUT_RECORD_SEPARATOR = undef; <$fh> };
     close $fh;
-    
+
     return $text;
 }
 
 sub _process_transmitter_log {
     my $fh = shift;
-   
+
     my $csv = Text::CSV_XS->new( { q/allow_whitespace/ => 1, } );
-    
-    my @column_names = @{$csv->getline($fh)};
+
+    my @column_names       = @{ $csv->getline($fh) };
     my $horizontal_records = {};
-    my $vertical_record = {};
-    $csv->bind_columns(\@{$vertical_record}{@column_names});
-    while ($csv->getline($fh)) {
+    my $vertical_record    = {};
+    $csv->bind_columns( \@{$vertical_record}{@column_names} );
+    while ( $csv->getline($fh) ) {
         my $time = $vertical_record->{'Time'};
-        my $date = $vertical_record->{'Date'} . '/' . localtime->year;
-        my $value = $vertical_record->{'Current Value'} + 0; # coerce this into a number
-        my $key = $vertical_record->{'Type of Signal'} . $vertical_record->{'Channel number'};
-        my $field_name = $CHANNELS->{$key}{'Description'} || qq/Channel $vertical_record->{'Channel number'}/;
+        my $date = $vertical_record->{'Date'} . q{/} . localtime->year;
+        my $value =
+          $vertical_record->{'Current Value'} + 0;   # coerce this into a number
+        my $key = $vertical_record->{'Type of Signal'}
+          . $vertical_record->{'Channel number'};
+        my $field_name = $CHANNELS->{$key}{'Description'}
+          || qq/Channel $vertical_record->{'Channel number'}/;
 
         my $timestamp = qq/$date $time/;
         $horizontal_records->{$timestamp}{$field_name} = $value;
@@ -219,33 +267,34 @@ sub _process_transmitter_log {
 }
 
 sub get_channels {
-    my $channels_file = shift;
+    my $channels_file         = shift;
     my $channels_config_final = {};
-    my $channels_config = Config::Tiny->read($channels_file);
-    foreach my $channel (keys %$channels_config) {
-        if ($channel =~ /^channel/ixsm) {
-            (my $channel_number = $channel) =~ s/channel ([[:alnum:]]+)/$1/ism;
-            $channels_config_final->{$channel_number} = $channels_config->{$channel};
+    my $channels_config       = Config::Tiny->read($channels_file);
+    foreach my $channel ( keys %{$channels_config} ) {
+        if ( $channel =~ /^channel/ixsm ) {
+            my ($channel_number) = $channel =~ m/channel\s+([[:alnum:]]+)/ixsm;
+            $channels_config_final->{ uc $channel_number } =
+              $channels_config->{$channel};
         }
     }
     return $channels_config_final;
 }
 
-
 sub get_configuration {
     my $config_file = shift;
-    my $config = Config::Tiny->read($CONFIG_FILE);
-    if (!$config) {
-        croak(qq/Failed to read configuration file $config_file: / . Config::Tiny->errstr);
+    my $config      = Config::Tiny->read($CONFIG_FILE);
+    if ( !$config ) {
+        croak( qq/Failed to read configuration file $config_file: /
+              . Config::Tiny->errstr );
     }
-    if ($config->{'_'}{'field_order'}) {
-        my @field_order = split /\s+/, $config->{'_'}{'field_order'};
+    if ( $config->{'_'}{'field_order'} ) {
+        my @field_order = split /\s+/xsm, $config->{'_'}{'field_order'};
         $config->{'_'}{'field_order'} = \@field_order;
     }
 
-    foreach my $key (keys %DEFAULTS) {
-        foreach my $subkey (keys %{$DEFAULTS{$key}}) {
-            $config->{$key}{$subkey} ||= clone($DEFAULTS{$key}{$subkey});
+    foreach my $key ( keys %DEFAULTS ) {
+        foreach my $subkey ( keys %{ $DEFAULTS{$key} } ) {
+            $config->{$key}{$subkey} ||= clone( $DEFAULTS{$key}{$subkey} );
         }
     }
     return $config;
